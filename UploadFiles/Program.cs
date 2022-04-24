ce4e9a04e80500b4d77da4a3f2d4157bb2a4f738
GreedNeSS <ruslan199126@gmail.com>
2022-04-24 21:36:13 +0300

    Added upload files example.

---
+++ UploadFiles/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run(async context =>
+{
+    var response = context.Response;
+    var request = context.Request;
+    response.ContentType = "text/html; charset=utf-8";
+
+    if (request.Path == "/upload" && request.Method == "POST")
+    {
+        IFormFileCollection files = request.Form.Files;
+            var uploadPath = $"{Directory.GetCurrentDirectory()}/uploads";
+            Directory.CreateDirectory(uploadPath);
+
+            foreach (var file in files)
+            {
+                string fullPath = $"{uploadPath}/{file.FileName}";
+
+                using (FileStream fs = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fs);
+                }
+            }
+        await response.WriteAsync("Файлы успешно загружены");
+    }
+    else
+    {
+        await response.SendFileAsync("html/index.html");
+    }
+});
+
+app.Run();
